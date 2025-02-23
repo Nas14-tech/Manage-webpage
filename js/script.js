b0b9a84b8972b0d2daa3be93daa6a07ab8741673
Abdullateef Nasirudeen <nasirudeenabdullateef3@gmail.com>
2025-02-23 10:31:54 +0100

    project commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const btn =document.getElementById('menu-btn');
+const btnNav = document.getElementById('menu');
+
+btn.addEventListener('click', ()=>{
+  btn.classList.toggle('open')
+  btnNav.classList.toggle('flex')
+  btnNav.classList.toggle('hidden')
+})
